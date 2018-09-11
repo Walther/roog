44ffa4632be8fff6f4cb988caae399e2022ab6c6
Veeti Haapsamo <veeti.haapsamo@gmail.com>
2018-09-10 20:44:28 -0700

    Update main, print values for 1hz 1 second of oscillator

--- src/main.rs
+++ src/main.rs
@@ -1,18 +1,17 @@
 mod oscillator;
 use oscillator::sin;
-use std::f32::consts::PI;
 
 fn main() {
     println!("Hello, roog!");
 
-    let hertz = PI / 2.0;
+    let hertz = 1.0;
     let mut time = 0.0;
     let mut value;
     println!("Oscillator values:");
-    for _ in 0..10 {
+    for _ in 0..100 {
         value = sin(hertz, time);
-        print!("{} ", value);
-        time += 0.1;
+        println!("{},{}", time, value);
+        time += 0.01;
     }
     println!("")
 }
